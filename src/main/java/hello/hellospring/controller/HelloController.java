70a9668a21d7f09b72eccdc2e0183e4cfe940d3c
kantata <melodics@nate.com>
2022-07-25 23:14:13 +0900

    Feat : ResponseBody Test
    SpringBoot Study

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,38 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello"; //return jsp template/+{ViewName}+.html
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-spring")
+    @ResponseBody //api 형태 -> 그대로 데이터를 넣어줌
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // 문자라서 그냥 리턴함 ViewResolver
+    }
+
+    @GetMapping("hello-api") // /hello-api?name=spring
+    @ResponseBody
+    public Hello hellpApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // 객체가 오면 return json HttpMessageConverter
+    }
+
+    static class Hello {
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
